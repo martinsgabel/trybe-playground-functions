41ead70be3862a713abf38d2b7164104b0dd1151
martins <gabrielamspinola@gmail.com>
2021-12-06 08:19:42 -0300

    Revisão Requisito 7

--- src/challenges.js
+++ src/challenges.js
@@ -70,14 +70,15 @@ function highestCount(numArray) {
 // Desafio 7
 function catAndMouse(mouse,cat1,cat2) {
   // seu código aqui
-  let cat1Distance = mouse - cat1;
-  let cat2Distance = mouse - cat2;
+  // consultei na internet uma forma de transformar qualquer resultado em positivo e encontrei a função Math.abs(x) em https://qastack.com.br/programming/4652104/convert-a-negative-number-to-a-positive-one-in-javascript#:~:text=Talvez%20voc%C3%AA%20deva%20declarar%3A%20positivo,%C3%A9%20o%20ex%2Dsinal%20X.&text=Multiplicar%20por%20(%2D1)%20%C3%A9,converter%20n%C3%BAmeros%20negativos%20em%20positivos.
+  let cat1Distance = Math.abs(mouse - cat1);
+  let cat2Distance = Math.abs(mouse - cat2);
   let resGato;
 
   if (cat1Distance > cat2Distance){
     resGato = "cat1";
     return resGato;
-  } else if (cat2Distance === cat1Distance){
+  } else if (cat2Distance == cat1Distance){
     resGato = "os gatos trombam e o rato foge";
     return resGato;
   } else {
